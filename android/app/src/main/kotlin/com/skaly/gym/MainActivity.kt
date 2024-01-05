64ed94beb085abcac6e5f3cca8d9b3ec0e69e3f8
pablopronsky <pronskypablo@gmail.com>
2024-01-05 18:35:39 -0300

    primer commit

---
+++ android/app/src/main/kotlin/com/skaly/gym/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.skaly.gym
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  const LoginPage({super.key})
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.green[300],
+      body: Column( children: [
+
+      ],
+      ),
+    );
+  }
+}
